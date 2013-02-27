bdc539f5764505a3e51c1aa27181ab938363ac6e
martin <martin@martin-desktop.(none)>
2013-02-27 13:28:43 +0100

    Dont fall back to asset pipeline if precompiled resource is missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 OBDComm::Application.configure do
+  config.assets.precompile += %w( *.js *.css )
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -15,7 +16,7 @@ OBDComm::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
