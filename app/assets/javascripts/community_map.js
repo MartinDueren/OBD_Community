019d4d0e1a0dcb6d5504a87425a622527aab1a78
MartinDueren <martin.dueren@gmail.com>
2013-07-09 15:45:59 +0200

    Added legend to community map

--- app/assets/javascripts/community_map.js
+++ app/assets/javascripts/community_map.js
@@ -38,21 +38,29 @@ function init() {
 function changeSensor(sensor){
     switch (sensor){
         case "avg_speed":
+            document.getElementById("legend_description").innerHTML="Showing average speed";
+            document.getElementById("legend_image").src="http://localhost:8080/geoserver/OBDComm/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=OBDComm:osm_roads&style=avg_speed&FORMAT=image/png&transparent=true&LEGEND_OPTIONS=forceRule:True;dx:0.2;dy:0.2;mx:0.2;my:0.2;fontStyle:borderColor:0000ff;border:true;fontColor:000000;fontSize:14";
             layer.mergeNewParams({styles:sensor});
             break;
         case "avg_rpm":
+            document.getElementById("legend_description").innerHTML="Showing average rpm";
+            document.getElementById("legend_image").src="http://localhost:8080/geoserver/OBDComm/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=OBDComm:osm_roads&style=avg_rpm&FORMAT=image/png&transparent=true&LEGEND_OPTIONS=forceRule:True;dx:0.2;dy:0.2;mx:0.2;my:0.2;fontStyle:borderColor:0000ff;border:true;fontColor:000000;fontSize:14";
             layer.mergeNewParams({styles:sensor});
             break;
         case "avg_co2": 
+            document.getElementById("legend_description").innerHTML="Showing average co2";
             layer.mergeNewParams({styles:sensor});
             break; 
         case "avg_consumption": 
+            document.getElementById("legend_description").innerHTML="Showing average consumption";
             layer.mergeNewParams({styles:sensor});
             break; 
         case "avg_standing_time": 
+            document.getElementById("legend_description").innerHTML="Showing average standing time";
             layer.mergeNewParams({styles:sensor});
             break; 
-        case "max_speed": 
+        case "max_speed":
+            document.getElementById("legend_description").innerHTML="Showing max speed"; 
             layer.mergeNewParams({styles:sensor});
             break;    
     }
